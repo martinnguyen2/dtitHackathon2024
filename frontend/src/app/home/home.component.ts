83727f038a524aa1c508536986d0befd0e682aab
Matej Harabin <matejharabin@kosickaakademia.sk>
2024-11-23 14:06:05 +0100

    edo čonka

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {ToolBarComponent} from '../components/tool-bar/tool-bar.component';
 import {ContentComponent} from '../components/content/content.component';
+import { QueryBarComponent } from "../components/query-bar/query-bar.component";
 
 @Component({
   selector: 'app-home',
@@ -9,8 +10,9 @@ import {ContentComponent} from '../components/content/content.component';
   templateUrl: './home.component.html',
   imports: [
     ToolBarComponent,
-    ContentComponent
-  ],
+    ContentComponent,
+    QueryBarComponent
+],
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
