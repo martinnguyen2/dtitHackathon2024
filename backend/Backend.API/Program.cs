a6aa267ea44d638c79243e7d90bcf0892a483bf4
BearX222 <tbalog102@outlook.com>
2024-11-23 16:33:05 +0100

    Fix CORS

--- backend/Backend.API/Program.cs
+++ backend/Backend.API/Program.cs
@@ -15,6 +15,19 @@ builder.Services.RegisterServices();
 
 builder.Services.AddScoped<IPythonExecuteService, PythonExecuteService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy  =>
+        {
+            policy.AllowAnyOrigin();
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+            policy.AllowCredentials();
+        }
+    );
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,5 +40,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapControllers();
+
+app.UseCors();
 
 app.Run();
