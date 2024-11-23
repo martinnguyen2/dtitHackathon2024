626f44a1b8f7b574b2d2f8794bb401023727024b
Matej Harabin <matejharabin@kosickaakademia.sk>
2024-11-23 19:28:34 +0100

    Dačo take jak inital commit

--- frontend/src/app/components/tool-bar/tool-bar.component.ts
+++ frontend/src/app/components/tool-bar/tool-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatasetsService } from '../../services/datasets.service';
 
 @Component({
   selector: 'app-tool-bar',
@@ -8,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class ToolBarComponent {
 
+  constructor(private datasetService: DatasetsService){}
+
+  public onFileSelected(event : any){
+
+    const file:File = event.target.files[0];
+    if(!file)
+      return;
+
+    const dataToSend = new FormData()
+
+    dataToSend.append("file",file)
+
+    this.datasetService.postDataset(dataToSend).subscribe(
+      next => console.log(next)
+    );
+  }
 }
