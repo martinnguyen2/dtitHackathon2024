3b51ddc3a0f317e47f833bafe2b9cb4b8a2a2b43
martin.nguyen <85219104+martinnguyen2@users.noreply.github.com>
2024-11-23 15:12:54 +0100

    Add service collection extensions

--- backend/Backend.API/Controllers/ChatPromptController.cs
+++ backend/Backend.API/Controllers/ChatPromptController.cs
@@ -1,4 +1,5 @@
 using Backend.API.DTOs;
+using Backend.API.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.API.Controllers
@@ -7,21 +8,22 @@ namespace Backend.API.Controllers
     [ApiController]
     public class ChatPromptController : ControllerBase
     {
+        private readonly IChatPromptService _chatPromptService;
+
+        public ChatPromptController(IChatPromptService chatPromptService)
+        {
+            _chatPromptService = chatPromptService;
+        }
+        
         [HttpPost("submit")]
         public ActionResult<string> SubmitPrompt(PromptDto promptDto) 
         {
-            /* in body
-            obj: {
-                public string prompt {get; set;}
-            }
-            */ 
-
             if (promptDto == null) 
             {
                 return BadRequest();
             }
             
-            return promptDto.Prompt;
+            return _chatPromptService.Submit(promptDto);
         }
     }
 }
